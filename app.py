4a16c4365af14e0b71c77c1a2d90a968ad5da1b8
christopher.lanham@cotiviti.com <christopher.lanham@cotiviti.com>
2026-01-29 00:37:47 +0000

    Added necessary files to deploy

--- app.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-st.set_page_config(layout="wide")
-
-st.header("Hello world!!!")
-+
-+# Bar Chart
-apps = st.slider("Number of apps", max_value=60, value=10)
-chart_data = pd.DataFrame({'y':[2 ** x for x in range(apps)]})
-st.bar_chart(chart_data, height=500, width=min(100+50*apps, 1000), 
